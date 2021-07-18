b37359a3ff92505dc0f4fe771db60d8cfc9aa3cd
Literally pie <78336842+Ilikepieitsgood@users.noreply.github.com>
2021-07-18 00:04:05 -0500

    Update egg.nim

--- egg.nim
+++ egg.nim
@@ -1,15 +1,15 @@
 import dimscord, asyncdispatch, times, options, os, parseutils, strutils
 
 let discord = newDiscordClient(getEnv("EGG_BOT_TOKEN"))
-var eggs = 0
+var new = 0
 
-if fileExists("eggs.txt"):
-  discard readFile("eggs.txt").parseInt(eggs)
+if fileExists("new.txt"):
+  discard readFile("new.txt").parseInt(new)
 
 proc onReady(s: Shard, r: Ready) {.event(discord).} =
   echo "Ready as " & $r.user
   await s.updateStatus(activity = ActivityStatus(
-    name: $eggs & " 🥚",
+    name: $new & " 👀",
     kind: atPlaying
   ).some, status = "online")
 
@@ -20,10 +20,10 @@ proc messageCreate(s: Shard, m: Message) {.event(discord).} =
     try:
       await discord.api.addMessageReaction(m.channelId, m.id, "🥚")
       await s.updateStatus(activity = ActivityStatus(
-        name: $eggs & " 🥚",
+        name: $new & " 👀",
         kind: atPlaying
       ).some, status = "online")
-      writeFile("eggs.txt", $eggs)
+      writeFile("new.txt", $new)
     except:
       discard #I don't care
 
